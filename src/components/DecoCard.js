b95788598c2f40cb6e6ddcffdaa487f0299e82af
liel0978 <linn.eliassen@telia.com>
2019-11-07 21:27:41 +0100

    style fixes

--- src/components/DecoCard.js
+++ src/components/DecoCard.js
@@ -1,11 +1,11 @@
 import React from 'react';  
 import {useSpring, animated} from 'react-spring';  
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 10, (x - window.innerWidth / 2) / 10, 1.1]
 const trans = (x, y, s) => `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`  
 
 function DeckOfCards() { 
-  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 550, friction: 30 } }))
   const number = Math.floor(Math.random() * Math.floor(21))
  
   return (
